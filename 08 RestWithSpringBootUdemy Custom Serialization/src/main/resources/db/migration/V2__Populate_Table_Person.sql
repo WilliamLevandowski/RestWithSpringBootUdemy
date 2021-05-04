0405cb6d8b2727120a71d7d2e2d9ade8e838b6c9
WilliamLevandowski <william.wski@gmail.com>
2021-05-04 11:56:28 -0300

    Versao 07,08 e 09

---
+++ 08 RestWithSpringBootUdemy Custom Serialization/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Jaragua do Sul - SC - Brasil', 'William', 'Male', 'Levandowski'),
+	(2, 'Jaraguá do Sul - Santa Catarina - Brasil', 'Kaine', 'Female', 'Levandowski'),
+	(5, 'Guaramirim - Santa Catarina - Brasil', 'Flavio', 'Male', 'Santos'),
+	(7, 'Jaraguá do Sul - Santa Catarina - Brasil', 'Fernanda', 'Female', 'Cardoso da Silva'),
+	(8, 'Jaraguá do Sul - Santa Catarina - Brasil', 'Pedro', 'Male', 'Paulo'),
+	(9, 'Jaraguá do Sul - Santa Catarina - Brasil', 'Marcos', 'Male', 'Paulo');
